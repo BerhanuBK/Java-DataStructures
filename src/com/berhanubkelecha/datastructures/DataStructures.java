77dc2cf6c482227e23218800c79bad50866774e3
Berhanu Borena Kelecha <BerhanuBorena@gmail.com>
2019-01-11 13:52:25 -0500

    Additional code to implement repetition and rray search

--- src/com/berhanubkelecha/datastructures/DataStructures.java
+++ src/com/berhanubkelecha/datastructures/DataStructures.java
@@ -3,8 +3,6 @@ package com.berhanubkelecha.datastructures;
 public class DataStructures {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		MyStack myStack=new MyStack(5);
 		
 		myStack.push(25);
@@ -17,6 +15,69 @@ public class DataStructures {
 		AddingNumbers addingNumbers=new AddingNumbers(5);
 		
 		System.out.println("The summation is"+addingNumbers.add());
+		
+		System.out.println("Working on Arrays");
+		
+		int[] Array=new int[50];
+		
+		for( int i=0; i<50; i++) {
+			Array[i]=10+(int)(10 *Math.random());
+		//	System.out.println(Array[i]);
+		}
+		
+		int searchNum=10+(int)(10 *Math.random());
+		
+		System.out.println(searchNum);
+				
+		boolean flag=false;
+		
+		for (int j=0;j<50;j++) {
+			if(Array[j]==searchNum)
+				System.out.println(searchNum+" is found at "+j);
+			flag=true;	
+		}
+		
+		if(!flag)
+			System.out.println("Not Found");	
+		
+		System.out.println("Finding the first repeating and non repeating letters in Arrays");
+		
+		String wordsRepeating="heWordsthatRepeat";
+		char chars[]=wordsRepeating.toCharArray();
+		
+		flag=false;
+		
+		char[] foundChars=new char[wordsRepeating.length()];
+		char[] nonfoundChars=new char[wordsRepeating.length()];
+		
+		for(int i=0; i<chars.length; i++){
+			flag=false;
+			for (int j=0; j<chars.length; j++){
+					if((chars[i]==chars[j])&&(j!=i)) {						
+						flag=true;
+						foundChars[i]=chars[i];
+					}					
+			}
+			
+			if(flag==false)
+				nonfoundChars[i]=chars[i];
+		}
+		
+		
+		for (int k=0;k<chars.length;k++)
+		{
+			if(foundChars[k] != Character.MIN_VALUE) {		
+				System.out.println("Repeating: the first repeating letter : "+foundChars[k]);
+				break;
+			}
+			
+		}
+		for (int k=0;k<chars.length;k++)
+		{
+			if(nonfoundChars[k] != Character.MIN_VALUE) {		
+				System.out.println("Non Repeating: the first non repeating letter :"+nonfoundChars[k]);
+				break;
+			}
+		}
 	}
-
 }
